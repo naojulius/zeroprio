063b0fd355e97c44dc7818631a193fd85e52b1a4
naojulius <naojulius.mg@gmail.com>
2020-08-06 18:03:58 +0300

    fixed index menu, footer, and media queries

--- js/radioButton.js
+++ js/radioButton.js
@@ -1,11 +1,15 @@
 var target = [$('#achat'), $('#location')];
 target[0].click(function () {
     $(this).addClass('underlined-white').removeClass('underlined-gray');
+    $(this).addClass('lato-bold').removeClass('lato-light');
+    $('#location').addClass('lato-light').removeClass('lato-bold');
     target[1].addClass('underlined-gray').removeClass('underlined-white');
     $("#type").val("achat");
 });
 target[1].click(function () {
     $(this).addClass('underlined-white').removeClass('underlined-gray');
+    $(this).addClass('lato-bold').removeClass('lato-light');
+    $('#achat').addClass('lato-light').removeClass('lato-bold');
     target[0].addClass('underlined-gray').removeClass('underlined-white');
     $("#type").val("location");
 });
